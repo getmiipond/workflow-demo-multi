f079de0288b7b9252c86cd1e82f2f99312e9e096
Jason Hansen <jhansen@deis.com>
2016-12-06 13:47:46 -0800

    feat(process): dump process information

--- Gemfile
+++ Gemfile
@@ -51,3 +51,4 @@ gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
 
 gem 'sidekiq'
 gem 'sinatra', '>= 2.0.0.beta2', require: false
+gem 'sys-proctable'
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,14 @@
+require 'sys/proctable'
+
 module ApplicationHelper
+  def process_info
+    process = Sys::ProcTable.ps($$)
+    {
+      "NOW": Time.now,
+      "COMM": process.comm,
+      "CWD": Dir.pwd,
+      "PID": process.pid,
+      "PPID": process.ppid,
+    }
+  end
 end
